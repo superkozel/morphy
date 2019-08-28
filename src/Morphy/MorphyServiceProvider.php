a33d52342ae0366739048615dee3c3662e403de5
Dmitriy Zykov <aesleep@ya.ru>
2019-08-28 03:50:55 +0300

    123

--- src/Morphy/MorphyServiceProvider.php
+++ src/Morphy/MorphyServiceProvider.php
@@ -8,14 +8,15 @@ class MorphyServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/morphy.php', 'morphy');
+        $configPath = __DIR__.'/../../config/morphy.php'
+        $this->mergeConfigFrom($configPath, 'morphy');
 
         if (function_exists('config_path')) {
             $publishPath = config_path('morphy.php');
         } else {
             $publishPath = base_path('config/morphy.php');
         }
-        $this->publishes([__DIR__ . '/../config/morphy.php' => $publishPath], 'config');
+        $this->publishes([$configPath => $publishPath], 'config');
 
         Morphy::setConfig(Config::get('morphy'));
     }
